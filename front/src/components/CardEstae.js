4957a7c2a14ebdcb89cfc3e0e2355cf6d0f33692
sasanzare <sasanzare75@gmail.com>
2022-01-09 09:59:57 +0330

    Add carousel

--- front/src/components/CardEstae.js
+++ front/src/components/CardEstae.js
@@ -1,9 +1,13 @@
+import Rate from './Rate'
+import React, { useState } from "react";
 function Card({img,title}) { 
+  const [rating, setRating] = useState(0);
   return (
         <div className="p-3 pt-4">
-        <div className="shadow-es text-center">
+        <div className="shadow-es text-center pb-2">
             <img src={img} className="w-100"/>
             <p>{title}</p>
+            <Rate rating={rating} onRating={(rate) => setRating(rate)} />
         </div>
 
     </div>
